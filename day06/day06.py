118e5bdc208750c14ba2a3ef6716b7fb22db8e66
lanxuexing <lanxuexing313wsr@163.com>
2026-03-04 16:32:07 +0800

    feat: 新增从1-100的奇数求和、整数求和及打印乘法口诀表功能

--- day06/day06.py
+++ day06/day06.py
@@ -34,3 +34,40 @@ for x in range(1, 101):
     if x % 2 == 0:
         total += x
 print(f'1-100的偶数和: {total}')
+
+
+print('---------------------------')
+
+"""
+从1-100的奇数求和
+
+Vseion: 1.0
+"""
+print(f'1-100的奇数和: {sum(range(1, 101, 2))}')
+
+print('---------------------------')
+
+"""
+从1到100的整数求和
+
+Vseion: 1.0
+"""
+total = 0
+i = 1
+while i < 101:
+    total += i
+    i += 1
+print(f'1-100的整数和: {total}')
+
+
+print('---------------------------')
+
+"""
+打印乘法口诀表
+
+Version: 1.0
+"""
+for i in range(1, 10):
+    for j in range(1, i + 1):
+        print(f'{i}×{j}={i * j}', end='\t')
+    print()
