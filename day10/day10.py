1562ee88035c56ebddca5e896849074e67cb724f
lanxuexing <lanxuexing313wsr@163.com>
2026-03-17 16:01:52 +0800

    feat: 新增day10.py文件，包含元组操作示例及性能测试

---
+++ day10/day10.py
@@ -0,0 +1,96 @@
+# 定义一个三元组
+t1 = (35, 12, 98)
+# 定义一个四元组
+t2 = ('甄姬', 45, True, '王者荣耀')
+
+# 查看变量的类型
+print(type(t1))  # <class 'tuple'>
+print(type(t2))  # <class 'tuple'>
+
+# 查看元组中元素的数量
+print(len(t1))  # 3
+print(len(t2))  # 4
+
+# 索引运算
+print(t1[0])    # 35
+print(t1[2])    # 98
+print(t2[-1])   # 王者荣耀
+
+# 切片运算
+print(t2[:2])   # ('甄姬', 45)
+print(t2[::3])  # ('甄姬', '王者荣耀')
+
+# 循环遍历元组中的元素
+for elem in t1:
+    print(elem)
+
+# 成员运算
+print(12 in t1)         # True
+print(99 in t1)         # False
+print('Hao' not in t2)  # True
+
+# 拼接运算
+t3 = t1 + t2
+print(t3)  # (35, 12, 98, '甄姬', 45, True, '王者荣耀')
+
+# 比较运算
+print(t1 == t3)            # False
+print(t1 >= t3)            # False
+print(t1 <= (35, 11, 99))  # False
+
+
+print('---------------------------')
+
+# 打包操作
+a = 1, 10, 100
+print(type(a))  # <class 'tuple'>
+print(a)        # (1, 10, 100)
+# 解包操作
+i, j, k = a
+print(i, j, k)  # 1 10 100
+
+print('---------------------------')
+
+a = 1, 10, 100, 1000
+i, j, *k = a
+print(i, j, k)        # 1 10 [100, 1000]
+i, *j, k = a
+print(i, j, k)        # 1 [10, 100] 1000
+*i, j, k = a
+print(i, j, k)        # [1, 10] 100 1000
+*i, j = a
+print(i, j)           # [1, 10, 100] 1000
+i, *j = a
+print(i, j)           # 1 [10, 100, 1000]
+i, j, k, *l = a
+print(i, j, k, l)     # 1 10 100 [1000]
+i, j, k, l, *m = a
+print(i, j, k, l, m)  # 1 10 100 1000 []
+
+
+print('---------------------------')
+
+a, b, *c = range(1, 10)
+print(a, b, c)
+a, b, c = [1, 10, 100]
+print(a, b, c)
+a, *b, c = 'hello'
+print(a, b, c)
+
+print('---------------------------')
+
+import timeit
+
+print('%.3f 秒' % timeit.timeit('[1, 2, 3, 4, 5, 6, 7, 8, 9]', number=10000000))
+print('%.3f 秒' % timeit.timeit('(1, 2, 3, 4, 5, 6, 7, 8, 9)', number=10000000))
+
+
+print('---------------------------')
+
+infos = ('甄姬', 45, True, '王者荣耀')
+# 将元组转换成列表
+print(list(infos))  # ['甄姬', 45, True, '王者荣耀']
+
+frts = ['apple', 'banana', 'orange']
+# 将列表转换成元组
+print(tuple(frts))  # ('apple', 'banana', 'orange')
