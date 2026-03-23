bf2c3cf5eea26b30f180e028defca907949bd2f0
lanxuexing <lanxuexing313wsr@163.com>
2026-03-23 18:50:25 +0800

    feat: 更新day4.py文件，重构组合数计算功能，新增阶乘函数以简化代码

--- day14/day4.py
+++ day14/day4.py
@@ -4,8 +4,10 @@
 Version: 1.0
 """
 
-m = int(input('m = '))
-n = int(input('n = '))
+# m = int(input('m = '))
+# n = int(input('n = '))
+m = 7
+n = 3
 # 计算m的阶乘
 fm = 1
 for num in range(1, m + 1):
@@ -20,3 +22,38 @@ for num in range(1, m - n + 1):
     fk *= num
 # 计算C(M,N)的值
 print(fm // fn // fk)
+
+print('---------------------------')
+
+
+"""
+输入m和n，计算组合数C(m,n)的值
+
+Version: 1.1
+"""
+
+
+# 通过关键字def定义求阶乘的函数
+# 自变量（参数）num是一个非负整数
+# 因变量（返回值）是num的阶乘
+def fac(num):
+    result = 1
+    for n in range(2, num + 1):
+        result *= n
+    return result
+
+
+m = int(input('m = '))
+n = int(input('n = '))
+# 计算阶乘的时候不需要写重复的代码而是直接调用函数
+# 调用函数的语法是在函数名后面跟上圆括号并传入参数
+print(fac(m) // fac(n) // fac(m - n))
+
+
+print('---------------------------')
+
+from math import factorial
+
+m = int(input('m = '))
+n = int(input('n = '))
+print(factorial(m) // factorial(n) // factorial(m - n))
